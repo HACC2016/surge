c836a331802cd5c62b9f1d9c89ade0e8ad051e2e
wata123 <Gina@Ginas-MacBook-Air.local>
2016-09-08 02:12:21 -1000

    Used javascript to make certain words act as links.

--- js/surge.js
+++ js/surge.js
@@ -1,4 +1,8 @@
 window.onload = function() {
   var me = document.getElementById("me");
   var gear = document.getElementById("gear");
+
+  me.onclick = function() {
+    window.open(surge-course-page/index.html);
+  }
 }
