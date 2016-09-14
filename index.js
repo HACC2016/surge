57ed821f6dd6f691928a0ebc4d20a3c49651a83c
sgnl <rayrfarias@gmail.com>
2016-09-14 09:08:51 -1000

    updates banner, :tada:

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const PORT = CONFIG[ENVIRONMENT].SERVER.PORT;
 
 Server.listen(PORT, _ => {
   process.stdout.write(`${Chalk.bgCyan(`
-${Chalk.bgRed(`    _____  _____  _____  _____  _____   `)}
-${Chalk.bgRed(`   |   __||  |  || __  ||   __||   __|  `)} ${Chalk.black.underline(`serv @ http://localhost:${PORT}`)}
-${Chalk.bgRed(`   |__   ||  |  ||    -||  |  ||   __|  `)}
-${Chalk.bgRed(`   |_____||_____||__|__||_____||_____|  `)} hacked by |> Gina |> Sean |> Ray; 2016 HACC
-${Chalk.bgRed(`                                        `)}`)}
-  `);
+${Chalk.bgRed(`    _____  _____  _____  _____  _____   ${Chalk.bgCyan(`                                                 `)}`)}
+${Chalk.bgRed(`   |   __||  |  || __  ||   __||   __|  `)} ${Chalk.black.underline(`serv @ http://localhost:${PORT}     `)}
+${Chalk.bgRed(`   |__   ||  |  ||    -||  |  ||   __|  ${Chalk.bgCyan(`                                                 `)}`)}
+${Chalk.bgRed(`   |_____||_____||__|__||_____||_____|  `)}${Chalk.bgCyan(` hacked by |> Gina |> Sean |> Ray; 2016 HACC     `)}
+${Chalk.bgRed(`                                        ${Chalk.bgCyan(`                                                 `)}`)}`)}
+                                                                                                                        `);
 });
