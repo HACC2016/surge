f0325295fe8673182328ac8d21cbe8fffa7d3ab2
sgnl <rayrfarias@gmail.com>
2016-09-14 11:46:36 -1000

    adds gulp-sourcemaps for generating scss -> css sourcemap files

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const Gulp = require('gulp');
 const Sass = require('gulp-sass');
 const Concat = require('gulp-concat');
+const SourceMaps = require('gulp-sourcemaps');
 
 Gulp.task('default', _ => {
 
@@ -15,7 +16,9 @@ Gulp.task('dev', [''], _ => {
 Gulp.task('sass', _ => {
   return Gulp.src('./server/scss/**/*.scss')
     .pipe(Sass().on('error', Sass.logError))
+    .pipe(SourceMaps.init())
     .pipe(Concat('styles.css'))
+    .pipe(SourceMaps.write('./maps'))
     .pipe(Gulp.dest('./server/public/css/'));
 });
 
