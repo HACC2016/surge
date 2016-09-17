1c6642f4e3ec3285cbc45932ab086fed7be54e1e
Gina W <watanabegina26@gmail.com>
2016-09-17 04:04:07 -1000

    Added gear icon.

--- app/surge-course-index/js/surge.js
+++ app/surge-course-index/js/surge.js
@@ -1,10 +1,15 @@
 window.onload = function() {
   var header = document.getElementById('header');
   var title = document.getElementById('title');
+  var vid = document.getElementById('home-video');
 
   function testScroll(ev){
       if(window.pageYOffset>400) {
         title.innerHTML = "Helloooooo";
   }
   window.onscroll = testScroll();
+
+  vid.onended = function() {
+    title.innerHTML = "HIHIHIHIHI";
+  }
 }
