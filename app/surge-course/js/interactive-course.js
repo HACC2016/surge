ea78d97122c01e68763930b2310d1c923f6bd80b
Gina W <watanabegina26@gmail.com>
2016-09-17 11:53:02 -1000

    Configuration of video source changes based on answer chosen (clicked) for initial question/answer branch.

--- app/surge-course/js/interactive-course.js
+++ app/surge-course/js/interactive-course.js
@@ -1,9 +1,14 @@
 window.onload = function() {
   var x = 'What?';
   var y = 'Good! What about you bruddah?';
-  var video = document.getElementById('video');
+
+  var video = document.querySelector('#video');
+  var source = document.querySelector('#video-source');
+
   var answer1 = document.createElement("button");
+  answer1.setAttribute("id", "answerNumber1");
   var answer2 = document.createElement("button");
+  answer2.setAttribute("id", "answerNumber2");
   var value1 = document.createTextNode(x);
   var value2 = document.createTextNode(y);
 
@@ -14,10 +19,16 @@ window.onload = function() {
 
     answer2.appendChild(value2);
     document.getElementById('question').appendChild(answer2);
-  }, false);
 
-  video.addEventListener("loadedmetadata", function() {
-    video.currentTime = video.duration - 4;
+    document.querySelector("answerNumber1");
+
+    answerNumber1.onclick = function() {
+      console.log("hello");
+      source.setAttribute('src', 'video1.mp4');
+      video.load();
+      video.play();
+    }
+
   }, false);
 
   video.load();
