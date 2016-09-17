74cf7f6bad4c664e853ce285de55310e2ce17074
Gina W <watanabegina26@gmail.com>
2016-09-17 13:11:29 -1000

    1st scenario video branch.

--- app/surge-course/js/interactive-course.js
+++ app/surge-course/js/interactive-course.js
@@ -1,6 +1,7 @@
 window.onload = function() {
   var x = 'What?';
   var y = 'Good! What about you bruddah?';
+  var y2 = 'Shootz!';
 
   var video = document.querySelector('#video');
   var source = document.querySelector('#video-source');
@@ -9,10 +10,23 @@ window.onload = function() {
   answer1.setAttribute("id", "answerNumber1");
   var answer2 = document.createElement("button");
   answer2.setAttribute("id", "answerNumber2");
+  var answer3 = document.createElement("button");
+  answer3.setAttribute("id", "answerNumber3");
+  var answer4 = document.createElement("button");
+  answer4.setAttribute("id", "answerNumber4");
   var value1 = document.createTextNode(x);
   var value2 = document.createTextNode(y);
+  var value3 = document.createTextNode(y2);
+  var value4 = document.createTextNode("The End");
+
+  // Removes an element from the document
+  function removeElement(elementId) {
+    var element = document.getElementById(elementId);
+    element.parentNode.removeChild(element);
+    }
 
   video.addEventListener("ended", function(e) {
+
     console.log("ended fired!");
     answer1.appendChild(value1);
     document.getElementById('question').appendChild(answer1);
@@ -21,13 +35,25 @@ window.onload = function() {
     document.getElementById('question').appendChild(answer2);
 
     document.querySelector("answerNumber1");
+    document.querySelector("answerNumber2");
 
     answerNumber1.onclick = function() {
       console.log("hello");
       source.setAttribute('src', 'video1.mp4');
       video.load();
       video.play();
-    }
+
+      removeElement('answerNumber1');
+      removeElement('answerNumber2');
+
+      video.addEventListener("ended", function(e) {
+        console.log("ended fired for scenario1!");
+        answer3.appendChild(value4);
+        document.getElementById('question').appendChild(answer3);
+        removeElement('answerNumber1');
+        removeElement('answerNumber2');
+      })
+      }
 
   }, false);
 
