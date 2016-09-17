1fc5b8c8a829c9b583752bf9705a9e2379f10af5
Gina W <watanabegina26@gmail.com>
2016-09-17 10:54:27 -1000

    Button spawn on video end in div below video.

--- app/surge-course/js/interactive-course.js
+++ app/surge-course/js/interactive-course.js
@@ -1,8 +1,13 @@
 window.onload = function() {
   var video = document.getElementById('video');
 
+
   video.addEventListener("ended", function(e) {
     console.log("ended fired!");
+    var answer = document.createElement("button");
+    var value = document.createTextNode("What?");
+    answer.appendChild(value);
+    document.getElementById('question').appendChild(answer);
   }, false);
 
   video.addEventListener("loadedmetadata", function() {
