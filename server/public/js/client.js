5a76ec54e4c013cb6f6e8b3f067d457ec23dd235
Gina W <watanabegina26@gmail.com>
2016-09-18 13:28:34 -1000

    Transfer of index/css/js files to server. Need to fix some layout bugss.

---
+++ server/public/js/client.js
@@ -0,0 +1,73 @@
+window.onload = function() {
+  // Video variables.
+  var video = document.querySelector('#video');
+  var source = document.querySelector('#video-source');
+
+  // Create button and append to div below video.
+  function addButton(value, id) {
+    var answer = document.createElement("button");
+    answer.setAttribute("id", id);
+    var value = document.createTextNode(value);
+    answer.appendChild(value);
+    document.getElementById('question').appendChild(answer);
+  }
+
+  // Removes an element from the document.
+  function removeElement(elementId) {
+    var element = document.getElementById(elementId);
+    element.parentNode.removeChild(element);
+    }
+
+  // On end of 1st video, append buttons to div.
+  video.addEventListener("ended", function(e) {
+
+    addButton("What?", "answerNumber1");
+    addButton("Oh Sup Brah!", "answerNumber2");
+
+    // On click of the answer, route to another video and remove the buttons.
+    answerNumber1.onclick = function() {
+      source.setAttribute('src', 'assets/videos/what.mp4');
+      video.load();
+      video.play();
+
+      removeElement('answerNumber1');
+      removeElement('answerNumber2');
+
+      video.addEventListener("ended", function(e) {
+        addButton("The End", "answerNumber3");
+
+        removeElement('answerNumber1');
+        removeElement('answerNumber2');
+      })
+      }
+
+    answerNumber2.onclick = function() {
+      source.setAttribute('src', 'assets/videos/ohsupbrah.mp4');
+      video.load();
+      video.play();
+
+      removeElement('answerNumber1');
+      removeElement('answerNumber2');
+
+      video.addEventListener("ended", function(e) {
+        addButton("Shootz!", "answerNumber4");
+
+        removeElement('answerNumber1');
+        removeElement('answerNumber2');
+
+        answerNumber4.onclick = function() {
+          source.setAttribute('src', 'assets/videos/shootz.mp4');
+          video.load();
+          video.play();
+          removeElement('answerNumber4');
+          addButton("The End", "answerNumber5");
+
+        }
+      })
+    }
+
+  }, false);
+
+  video.load();
+  video.play();
+}
