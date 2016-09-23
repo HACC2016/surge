85f1513c4e884ecc9ca5465c4b5ff2f55d27c0c3
Gina W <watanabegina26@gmail.com>
2016-09-23 00:55:23 -1000

    Styled analyze page.

--- server/public/js/analytics.js
+++ server/public/js/analytics.js
@@ -1,39 +1,24 @@
 window.onload = function() {
   var ctx = document.getElementById('myChart');
   var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'radar',
     data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      labels: ["Understanding", "Conversation", "Collaboration", "Mannerisms", "Eloquence", "Honesty"],
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: 'Strengths',
+        data: [20, 35, 43, 12, 20, 33],
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(254, 234, 123, 0.2)',
-          'rgba(21, 234, 23, 0.2)',
-          'rgba(45, 75, 88, 0.2)',
-          'rgba(26, 89, 66, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(254, 234, 123, 1)',
-          'rgba(21, 234, 23, 1)',
-          'rgba(45, 75, 88, 1)',
-          'rgba(26, 89, 66, 1)'
+          'rgba(79, 196, 177, 0.7)',
         ],
         borderWidth: 1
       }]
     },
     options: {
       scales: {
-        yAxes: [{
           ticks: {
             beginAtZero: true
           }
-        }]
+        }
       }
-    }
-  });
-}
+    })
+  };
