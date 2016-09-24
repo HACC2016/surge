b91f2b03c3ae14051574c31a2c104af19803fe8c
Sean Nakamura <seanenakamura@gmail.com>
2016-09-23 22:07:23 -1000

    fixes links

--- server/public/js/client.js
+++ server/public/js/client.js
@@ -28,12 +28,8 @@ window.onload = function() {
 
     // On click of the answer, route to another video and remove the buttons.
     answerNumber1.onclick = function() {
-<<<<<<< HEAD
       tempBar.style.height = "478px";
-      source.setAttribute('src', 'assets/videos/what.mp4');
-=======
       source.setAttribute('src', 'https://dl.dropboxusercontent.com/content_link/TZn9kvMFOCsrRvQzvbZkvRViHdPzyYmHNGkbxq4k0clQT0LwPcFOpBiwMfwenf9E/file');
->>>>>>> 5e20a33f60d63bd1eb0e4d6501c4f863a8d807a7
       video.load();
       video.play();
 
@@ -49,12 +45,8 @@ window.onload = function() {
       }
 
     answerNumber2.onclick = function() {
-<<<<<<< HEAD
       tempBar.style.height = "220px";
-      source.setAttribute('src', 'assets/videos/ohsupbrah.mp4');
-=======
       source.setAttribute('src', 'https://dl.dropboxusercontent.com/content_link/eDr2a5LY9HEi9MtaJtsLlrfdkmHfJMY7vTNG840DAy9n6AtOvIt3PtvjUyvgaEIR/file');
->>>>>>> 5e20a33f60d63bd1eb0e4d6501c4f863a8d807a7
       video.load();
       video.play();
 
@@ -68,12 +60,8 @@ window.onload = function() {
         removeElement('answerNumber2');
 
         answerNumber4.onclick = function() {
-<<<<<<< HEAD
           tempBar.style.height = "2px";
-          source.setAttribute('src', 'assets/videos/shootz.mp4');
-=======
           source.setAttribute('src', 'https://dl.dropboxusercontent.com/content_link/m2Z9tMUXNbcxr3XW42Ggc3MXJQ0vMSiymw0KooQ05RRrmXbF7bRvezCXXbZTQUEA/file');
->>>>>>> 5e20a33f60d63bd1eb0e4d6501c4f863a8d807a7
           video.load();
           video.play();
           removeElement('answerNumber4');
